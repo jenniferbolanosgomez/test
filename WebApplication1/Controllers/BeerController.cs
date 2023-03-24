37ea5bba43ed626ad74a50823dc74a5a39822013
Jennifer BOLANOS GOMEZ <jennifer.bolanos@amadeus.com>
2023-03-24 15:44:32 -0500

    test

---
+++ WebApplication1/Controllers/BeerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+
+        List<Beer> Beers = new List<Beer>()
+        {
+           new Beer() {Id=1 , Name = "Corona"},
+           new Beer() {Id=2 , Name = "aguila"},
+           new Beer() {Id=3 , Name = "budWeiser"},
+        };
+
+
+        [HttpGet]
+        public ActionResult<Beer> Get(int Id) {
+
+            var beer = Beers.Where(d => d.Id == Id).FirstOrDefault();
+            if (beer == null) return NotFound();
+            return beer;
+        }
+    }
+
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
